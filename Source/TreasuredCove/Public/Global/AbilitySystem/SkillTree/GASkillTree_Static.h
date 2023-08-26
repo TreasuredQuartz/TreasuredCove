f55f964090865a62044e4dede249044dd439adfd
Brandon Biggs <brandonbiggs15@gmail.com>
2023-08-26 01:13:41 -0400

    index on master: b331a3b Items: - Fixed various bugs regarding collision, UI, and animation.

---
+++ Source/TreasuredCove/Public/Global/AbilitySystem/SkillTree/GASkillTree_Static.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Global/AbilitySystem/SkillTree/GASkillTree.h"
+#include "GASkillTree_Static.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TREASUREDCOVE_API UGASkillTree_Static : public UGASkillTree
+{
+	GENERATED_BODY()
+	
+};
