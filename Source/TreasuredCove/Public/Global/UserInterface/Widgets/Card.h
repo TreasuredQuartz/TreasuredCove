c65b444736b88f981315df0c4d8f2c13e7a2457c
Brandon Biggs <brandonbiggs15@gmail.com>
2025-04-14 00:16:01 -0400

    Successful Card UI Pass
    Created a base blueprint prototype using the card styling I was using across the new menu.
    
    Created a c++ slate equivalent placeholder, for eventually polishing into a base widget similar to UButton.
    
    ManaBar expanded into multiple placeholder crystals.
    
    NewPauseMenu blur increased and color changed to black.
    
    Need to attach relevant character information to new UI (Name, Race, Class etc).

---
+++ Source/TreasuredCove/Public/Global/UserInterface/Widgets/Card.h
@@ -0,0 +1,17 @@
+// Copyright 2018 - 2023 B. M. Biggs. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ContentWidget.h"
+#include "Card.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TREASUREDCOVE_API UCard : public UContentWidget
+{
+	GENERATED_BODY()
+	
+};
